c690f5f55ebdc014aaf3028c80caf173ca08ba7e
Dist Bit <adp01@pm.me>
2024-03-14 17:51:57 +0300

    receive.py receive.sh

--- receive.py
+++ receive.py
@@ -1,13 +1,21 @@
 import os
+import time
 import subprocess
 import re
+import json
+import threading
 from urllib.parse import urlparse
 import sys
 from util import *
 import requests
+import websocket
+import pysnooper
+from dotenv import load_dotenv
 
 sys.path.append(getConfig()["convertLinksDir"])
 from convertLinks import main
+
+load_dotenv()
 
 
 def copy_to_clipboard(input_string):
@@ -25,16 +33,58 @@ def find_urls_in_text(text):
     return url_pattern.findall(text)
 
 
-# Fetch data from environment variable
-data = os.environ.get("message", "")
-if data == "You received a file: attachment.txt":
-    id = os.environ.get("id", "")
-    if not id:
-        data = "No attachment found."
-    else:
-        attachmentUrl = "https://ntfy.sh/file/" + id + ".txt"
-        data = requests.get(attachmentUrl).text
+def on_message(ws, message):
+    data = json.loads(message)
+    if "message" in data:
+        message_data = data["message"]
+        if message_data == "You received a file: attachment.txt":
+            id = data.get("id", "")
+            if not id:
+                message_data = "No attachment found."
+            else:
+                attachmentUrl = "https://ntfy.sh/file/" + id + ".txt"
+                message_data = requests.get(attachmentUrl).text
+        copy_to_clipboard(message_data)
+        main(message_data, True, False)
 
-print("it worked", data)
-copy_to_clipboard(data)
-main(data, True, False)
+
+def on_error(ws, error):
+    print(f"WebSocket error: {error}")
+
+
+def on_close(ws, close_status_code, close_msg):
+    print("WebSocket connection closed")
+
+
+def on_open(ws):
+    print("WebSocket connection opened")
+
+
+@pysnooper.snoop()
+def main():
+    topic = os.getenv("NTFY_RECEIVE_TOPIC")
+    if not topic:
+        print("Topic name not found in .env file. Please set the NTFY_TOPIC variable.")
+        sys.exit(1)
+
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp(
+        f"wss://ntfy.sh/{topic}/ws",
+        on_message=on_message,
+        on_error=on_error,
+        on_close=on_close,
+    )
+    ws.on_open = on_open
+    ws_thread = threading.Thread(target=ws.run_forever)
+    ws_thread.daemon = True
+    ws_thread.start()
+
+    try:
+        while True:
+            time.sleep(60)
+    except KeyboardInterrupt:
+        ws.close()
+
+
+if __name__ == "__main__":
+    main()
--- receive.sh
+++ receive.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-export XAUTHORITY=`ls  /run/user/1000/.* | grep mutt`;
-export DISPLAY=:0
-# Get the directory of the currently executing script
+source /home/pimania/dev/guiFromCron/crongui.sh
 script_dir="$(dirname "$0")"
-
-# Execute the Python script
 python3 "$script_dir/receive.py"
